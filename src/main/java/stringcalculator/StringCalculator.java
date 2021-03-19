a24996f0baf4af680b71ed1f019f90c7aee1ddfe
Ola Lindgård <ola.lindgard@iths.se>
2021-03-19 11:38:05 +0100

    created test for add first step

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,4 +1,20 @@
 package stringcalculator;
 
 public class StringCalculator {
+
+
+    public int add(String input) {
+        String[] array = input.split(",");
+        int sum = 0;
+        if(array.length > 3)
+            array = new String[0];
+
+        for(String s : array) {
+            if(s.isEmpty())
+                return 0;
+            sum += Integer.parseInt(s);
+        }
+        System.out.println(sum);
+        return sum;
+    }
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,9 +1,20 @@
 package stringcalculator;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
 
+    @Test
+    void testAdd() {
 
+        StringCalculator stringCalculator = new StringCalculator();
+
+        String input = "";
+        assertThat(0).isEqualTo(stringCalculator.add(input));
+
+    }
 
 }
