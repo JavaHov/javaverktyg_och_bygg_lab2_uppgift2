4a3aff9fb5b0ba25643d1d29591da99058c65d1b
Ola Lindgård <ola.lindgard@iths.se>
2021-03-23 14:53:37 +0100

    Test: Created test file, first commit.

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,14 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class EmployeeManagerTest {
 
+    @Test
+    void payEmployees() {
+
+
+    }
 }
